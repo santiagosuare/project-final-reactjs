96b510f4086a3c7332f7b67c42925f7f484bf172
Santiago Suarez <santiagosuarez@Air-de-Santiago.fibertel.com.ar>
2021-09-09 01:23:26 -0300

    Componentes I v2

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,20 @@
-
+import NavDropdown  from "react-bootstrap/NavDropdown";
+import Dropdown from "react-bootstrap/Dropdown";
 
 export const ItemListContainer = (props) => {
     return (
         <div>
-            <ul>
-                <li>{props.catalogo[0]}</li>
-                <li>{props.catalogo[1]}</li>
-                <li>{props.catalogo[2]}</li>
-                <li>{props.catalogo[3]}</li>
-            </ul>
+            <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Catalogo
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">{props.catalogo[0]}</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">{props.catalogo[1]}</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">{props.catalogo[2]}</Dropdown.Item>
+            </Dropdown.Menu>
+            </Dropdown>
         </div>
     )
 }
